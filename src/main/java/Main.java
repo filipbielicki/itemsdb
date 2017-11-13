db8f92222b47ce0dfcbb9654806813a2caad7fd7
filipbielicki <30906805+filipbielicki@users.noreply.github.com>
2017-11-13 11:03:08 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import dao.ItemDao;
 import dao.ItemDaoImpl;
 import dao.exceptions.EmptyTableException;
 import dao.exceptions.ItemNotFoundException;
@@ -6,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws EmptyTableException, ItemNotFoundException {
 
-        ItemDaoImpl itemDao = new ItemDaoImpl();
+        ItemDao itemDao = new ItemDaoImpl();
 
     }
 }
